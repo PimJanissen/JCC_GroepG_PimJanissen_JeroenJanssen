d58a59c1db43e588c8c99a98c9c0739b53e9a9c6
jehama <koekoe631@hotmail.com>
2016-02-23 11:30:58 +0100

    toevoeging project

---
+++ GroepG.Opdracht1.VerzamelApp/src/groepg/opdracht1/verzamelapp/Set.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package groepg.opdracht1.verzamelapp;
+
+/*
+ * @autor Pim Janissen
+ * @author Jeroen Janssen
+ */
+
+public class Set {
+ /*
+ * Fields
+ */
+private String naam;
+
+private 
+}
