55595d27e2bfd4b10f624f5de468b7162884172a
jehama <koekoe631@hotmail.com>
2016-02-28 23:48:57 +0100

    fix string

--- GroepG.Opdracht1.VerzamelApp/src/groepg/opdracht1/verzamelapp/Main.java
+++ GroepG.Opdracht1.VerzamelApp/src/groepg/opdracht1/verzamelapp/Main.java
@@ -14,47 +14,44 @@ import java.util.Date;
  *
  * @author gebruiker-pc
  */
-public class Main
-{
+public class Main {
 
     private static ArrayList<Set> sets;
 
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args)
-    {
+    public static void main(String[] args) {
         sets = new ArrayList<>();
         sets.add(createSet());
         showSets();
     }
 
-    private static Set createSet()
-    {
+    private static Set createSet() {
         Set set = new Set("Bierdopjes", new Date());
         SimpleDateFormat dateFormat = new SimpleDateFormat("dd-mm-yyyy");
         SimpleDateFormat yearFormat = new SimpleDateFormat("yyyy");
 
-        try
-        {
+        try {
             set.addVoorwerp(new Bierdopje("Heineken", dateFormat.parse("12-01-2016")));
             set.addVoorwerp(new Bierdopje("Hertog Jan", dateFormat.parse("15-8-2015")));
             set.addVoorwerp(new Postzegel(12, 25, yearFormat.parse("2011")));
             set.addVoorwerp(new Postzegel(18, 13, yearFormat.parse("1995")));
             set.addVoorwerp(new Postzegel(8, 20, yearFormat.parse("2001")));
-        } 
-        catch (ParseException e)
-        {
+        } catch (ParseException e) {
             System.out.printf("De testdata kon niet worden toegevoegd door de volgende fout: %s", e.toString());
         }
 
         return set;
     }
 
-    private static boolean showSets()
-    {
+    private static boolean showSets() {
+        int count = 0;
+        for (Set item : sets) {
+            System.out.printf("set %s %s \n", count , sets.get(count).getNaam());
+                        count ++;
+        }
 
-        System.out.printf("these are your sets %s", sets);
         return true;
     }
 }
