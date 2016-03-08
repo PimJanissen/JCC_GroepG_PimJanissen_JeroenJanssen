cb15a1ee11b822927c5a85332460a29d70519c67
jehama <koekoe631@hotmail.com>
2016-03-08 12:17:27 +0100

    mediator uitgebreid met postzegel

--- GroepG.Opdracht1.VerzamelApp/src/groepg/opdracht1/verzamelapp/DatabaseMediator.java
+++ GroepG.Opdracht1.VerzamelApp/src/groepg/opdracht1/verzamelapp/DatabaseMediator.java
@@ -47,16 +47,20 @@ public class DatabaseMediator
             try
             {
                 Statement statement = this.connection.createStatement();
-                ResultSet resultSet = statement.executeQuery("SELECT s.ID AS S_ID, s.naam AS S_NAAM, s.jaar AS S_JAAR, b.ID AS B_ID, b.uitgavejaar AS B_UITGAVEJAAR, b.merk AS B_MERK\n"
+                ResultSet resultSetBier = statement.executeQuery("SELECT s.ID AS S_ID, s.naam AS S_NAAM, s.jaar AS S_JAAR, b.ID AS B_ID, b.uitgavejaar AS B_UITGAVEJAAR, b.merk AS B_MERK\n"
                         + "FROM setjes s\n"
                         + "LEFT JOIN bierdopje b ON b.set_ID = s.ID\n"
                         + "ORDER BY s.ID, b.ID;");
+                ResultSet resultSetPostZegel = statement.executeQuery("SELECT s.ID AS S_ID, s.naam AS S_NAAM, s.jaar AS S_JAAR, p.ID AS P_ID, p.uitgavejaar AS P_UITGAVEJAAR, p.lengte AS P_LENGTE, p.breedte AS P_BREEDTE\n"
+                        + "FROM setjes s\n"
+                        + "LEFT JOIN postzegel p ON p.set_ID = s.ID\n"
+                        + "ORDER BY s.ID, p.ID;");
 
                 ArrayList<Set> sets = new ArrayList<>();
                 
-                while (resultSet.next())
+                while (resultSetBier.next())
                 {
-                    String setNaam = resultSet.getString("S_NAAM");
+                    String setNaam = resultSetBier.getString("S_NAAM");
                     
                     Set set = null;
                     
@@ -70,11 +74,42 @@ public class DatabaseMediator
                     
                     if (set == null)
                     {
-                        Date setJaar = resultSet.getDate("S_JAAR");
+                        Date setJaar = resultSetBier.getDate("S_JAAR");
                         set = new Set(setNaam, setJaar);
+                        sets.add(set);
                     }
                     
-                    Bierdopje bierdopje = new Bierdopje();
+                    Date uitgaveJaar = resultSetBier.getDate("B_UITGAVEJAAR");
+                    String merk = resultSetBier.getString("B_MERK");
+                    Bierdopje bierdopje = new Bierdopje(merk, uitgaveJaar);
+                    set.addVoorwerp(bierdopje);
+                }
+                
+                while (resultSetPostZegel.next())
+                {
+                    String setNaam = resultSetPostZegel.getString("S_NAAM");
+                    
+                    Set set = null;
+                    
+                    for (Set s : sets)
+                    {
+                        if (s.getNaam().equals(setNaam))
+                        {
+                            set = s;
+                        }
+                    }
+                    
+                    if (set == null)
+                    {
+                        Date setJaar = resultSetPostZegel.getDate("S_JAAR");
+                        set = new Set(setNaam, setJaar);
+                        sets.add(set);
+                    }
+                    Date uitgaveJaar = resultSetPostZegel.getDate("P_UITGAVEJAAR");
+                    int lengte = resultSetPostZegel.getInt("P_LENGTE");
+                    int breedte = resultSetPostZegel.getInt("P_BREEDTE");
+                    Postzegel postzegel = new Postzegel(lengte, breedte, uitgaveJaar);
+                    set.addVoorwerp(postzegel);
                 }
 
                 return sets;
