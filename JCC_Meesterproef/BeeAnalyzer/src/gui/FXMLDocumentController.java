407f2d09b844bc7ccf4bbae1705d0878201f4b7e
jehama <koekoe631@hotmail.com>
2016-03-22 11:06:18 +0100

    GUI bijgewerkt en verbonden met classes

--- JCC_Meesterproef/BeeAnalyzer/src/gui/FXMLDocumentController.java
+++ JCC_Meesterproef/BeeAnalyzer/src/gui/FXMLDocumentController.java
@@ -7,21 +7,83 @@ package gui;
 
 import java.net.URL;
 import java.util.ResourceBundle;
-import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 
 /**
  *
  * @author gebruiker-pc
  */
-public class FXMLDocumentController implements Initializable
-{    
+public class FXMLDocumentController implements Initializable {
+
+    @FXML
+    private Label lblSecondarySpecies;
+
+    @FXML
+    private Label lblPrimarySpecies;
+
+    @FXML
+    private Label lblAverage;
+
+    @FXML
+    private TextField tfSecondaryFertility;
+
+    @FXML
+    private TextField tfPrimaryFertility;
+
+    @FXML
+    private TextField tfSecondaryPollination;
+
+    @FXML
+    private TextField tfPrimaryLifespan;
+
+    @FXML
+    private TextField tfPrimaryPollination;
+
+    @FXML
+    private TextField tfPrimarySpecies;
+
+    @FXML
+    private Label lblSecondaryPollination;
+
+    @FXML
+    private TextField tfSecondaryLifespan;
+
+    @FXML
+    private Label lblFoundingYear;
+
+    @FXML
+    private TextField tfBeeCount;
+
+    @FXML
+    private Label lblPrimaryFertility;
+
+    @FXML
+    private Label lblPrimaryLifespan;
+
+    @FXML
+    private Label lblSecondaryLifespan;
+
+    @FXML
+    private Label lblPrimaryPollination;
+
+    @FXML
+    private TextField tfFoundingYear;
+
+    @FXML
+    private Label lblBeeCount;
+
+    @FXML
+    private TextField tfSecondarySpecies;
+
+    @FXML
+    private Label lblSecondaryFertility;
+
     @Override
-    public void initialize(URL url, ResourceBundle rb)
-    {
+    public void initialize(URL url, ResourceBundle rb) {
         // TODO
-    }    
-    
+    }
+
 }
